1aa15f4f7df22706d277afae2e3193274e500918
rachosuar <rachosuar@gmail.com>
2022-02-05 15:53:53 -0300

    second commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '51ef8808-5792-406f-867c-a199bfcc6b18';
 export const MODAL_CLOSE_SEC = 2.5;
